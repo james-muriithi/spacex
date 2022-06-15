de27baa85a3a97508d2ea1f1ade55e491be951c8
james-muriithi <muriithijames556@gmail.com>
2022-06-15 23:50:31 +0300

    added counter up

--- src/components/sections/falcon9/Statistics.vue
+++ src/components/sections/falcon9/Statistics.vue
@@ -3,19 +3,43 @@
     <div class="grid grid-cols-3 gap-5 sm:gap-4">
       <div class="text-center">
         <span>
-          <h2 class="text-5xl sm:text-7xl lg:text-9xl mb-3">152</h2>
+          <h2 class="text-5xl sm:text-7xl lg:text-9xl mb-3">
+            <ICountUp
+              :startVal="0"
+              :endVal="152"
+              :decimals="0"
+              :duration="3"
+              :options="{}"
+            />
+          </h2>
           <h4 class="text-xs sm:text-xl uppercase">total launches</h4>
         </span>
       </div>
       <div class="text-center">
         <span>
-          <h2 class="text-5xl sm:text-7xl lg:text-9xl mb-3">116</h2>
+          <h2 class="text-5xl sm:text-7xl lg:text-9xl mb-3">
+            <ICountUp
+              :startVal="0"
+              :endVal="116"
+              :decimals="0"
+              :duration="3"
+              :options="{}"
+            />
+          </h2>
           <h4 class="text-xs sm:text-xl uppercase">total landings</h4>
         </span>
       </div>
       <div class="text-center">
         <span>
-          <h2 class="text-5xl sm:text-7xl lg:text-9xl mb-3">95</h2>
+          <h2 class="text-5xl sm:text-7xl lg:text-9xl mb-3">
+            <ICountUp
+              :startVal="0"
+              :endVal="95"
+              :decimals="0"
+              :duration="3"
+              :options="{}"
+            />
+          </h2>
           <h4 class="text-xs sm:text-xl uppercase">total reflights</h4>
         </span>
       </div>
@@ -24,7 +48,10 @@
 </template>
 
 <script>
+import ICountUp from "vue-countup-v3";
+
 export default {
   name: "Statistics",
+  components: { ICountUp },
 };
 </script>
