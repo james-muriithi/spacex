0820785b0261fbbb2fbe20acbaa57da094511c4a
james-muriithi <muriithijames556@gmail.com>
2022-06-16 00:19:53 +0300

    added falcon description section

---
+++ src/components/sections/falcon9/FalconDescription.vue
@@ -0,0 +1,19 @@
+<template>
+  <PageBackground
+    :mobileBgImage="mobileBgImage"
+    :desktopBgImage="desktopBgImage"
+  >
+  </PageBackground>
+</template>
+
+<script>
+import PageBackground from '@/components/common/PageBackground.vue';
+export default {
+  name: "FalconDescription",
+  components: { PageBackground },
+  created() {
+    this.mobileBgImage = "/images/f9-lines-mobile.webp";
+    this.desktopBgImage = "/images/f9-lines-desktop.webp";
+  },
+};
+</script>
--- src/views/Falcon.vue
+++ src/views/Falcon.vue
@@ -1,13 +1,15 @@
 <template>
     <FalconBanner />
     <Statistics />
+    <FalconDescription />
 </template>
 
 <script>
 import FalconBanner from '@/components/sections/falcon9/FalconBanner.vue'
 import Statistics from '@/components/sections/falcon9/Statistics.vue'
+import FalconDescription from '@/components/sections/falcon9/FalconDescription.vue'
 export default {
-  components: { FalconBanner, Statistics },
+  components: { FalconBanner, Statistics, FalconDescription },
     name: "Falcon"
 }
 </script>
