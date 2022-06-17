25f3c9a50e7169aa4d17e0d080e3420345ef7d15
james-muriithi <muriithijames556@gmail.com>
2022-06-18 02:08:19 +0300

    added falcon gallery section

---
+++ src/components/sections/falcon9/FalconInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-dark py-16">
+    <div class="flex justify-center h-28 mb-6">
+      <span class="border-r"></span>
+    </div>
+    <div class="text-center mb-6">
+      <span class="text-gray-400 text-sm">
+        For information about our launch services, contact
+        <router-link target="_blank" class="text-white" to="mailto:sales@spacex.com">
+          sales@spacex.com
+        </router-link>
+      </span>
+    </div>
+    <div class="flex justify-center">
+        <section-button class="mr-6 text-white px-4">
+            Download users guide
+        </section-button>
+
+        <section-button class="text-white px-4">
+            capabilities and services
+        </section-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import SectionButton from '../../common/SectionButton.vue';
+export default {
+  components: { SectionButton },
+  name: "FalconInfo",
+};
+</script>
--- src/views/Falcon.vue
+++ src/views/Falcon.vue
@@ -4,6 +4,8 @@
     <FalconDescription />
     <FalconFlight />
     <FalconEngines />
+    <FalconGallery />
+    <FalconInfo />
 </template>
 
 <script>
@@ -12,8 +14,10 @@ import Statistics from '@/components/sections/falcon9/Statistics.vue'
 import FalconDescription from '@/components/sections/falcon9/FalconDescription.vue'
 import FalconFlight from '../components/sections/falcon9/FalconFlight.vue'
 import FalconEngines from '../components/sections/falcon9/FalconEngines.vue'
+import FalconGallery from '../components/sections/falcon9/FalconGallery.vue'
+import FalconInfo from '../components/sections/falcon9/FalconInfo.vue'
 export default {
-  components: { FalconBanner, Statistics, FalconDescription, FalconFlight, FalconEngines },
+  components: { FalconBanner, Statistics, FalconDescription, FalconFlight, FalconEngines, FalconGallery, FalconInfo },
     name: "Falcon"
 }
 </script>
